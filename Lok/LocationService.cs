2d2f44849bd7582756a4fc0b209fdaa0c2698fd8
Ibnu Daru Aji <ibnu.d.a@student.uns.ac.id>
2016-01-24 20:12:40 +0700

    Added some implemented methods. Before black out happen.

--- Lok/LocationService.cs
+++ Lok/LocationService.cs
@@ -14,6 +14,9 @@ using Android.Gms.Location;
 using Android.Gms.Common;
 using Android.Gms.Common.Apis;
 using Android.Locations;
+using Android.Util;
+using Java.Text;
+using Java.Util;
 using ILocationListener = Android.Gms.Location.ILocationListener;
 
 // using Android.Gms.Location;
@@ -23,32 +26,65 @@ namespace Lok
     public class LocationService : Service, GoogleApiClient.IConnectionCallbacks,
         GoogleApiClient.IOnConnectionFailedListener, ILocationListener
     {
+        private static readonly string TAG = "LocationService";
+
         private string _defaultUploadWebsite;
         private bool _currentlyProcessLocation = false;
+        private LocationRequest _locationRequest;
+        private GoogleApiClient _googleApiClient;
 
         public override IBinder OnBind(Intent intent)
         {
-            throw new NotImplementedException();
+            return null;
         }
 
         public void OnConnected(Bundle connectionHint)
         {
-            throw new NotImplementedException();
+            Log.Debug(TAG, "OnConnected");
+
+            _locationRequest = LocationRequest.Create();
+            _locationRequest.SetInterval(1000);
+            _locationRequest.SetFastestInterval(1000);
+            _locationRequest.SetPriority(LocationRequest.PriorityHighAccuracy);
+            LocationServices.FusedLocationApi.RequestLocationUpdates(_googleApiClient, _locationRequest, this);
         }
 
         public void OnConnectionSuspended(int cause)
         {
-            throw new NotImplementedException();
+            Log.Debug(TAG, "OnConnectionSuspended");
         }
 
         public void OnConnectionFailed(ConnectionResult result)
         {
-            throw new NotImplementedException();
+            Log.Error(TAG, "OnConnectionFailed");
+            StopLocationUpdates();
+            StopSelf();
+        }
+
+        private void StopLocationUpdates()
+        {
+            if (_googleApiClient != null && _googleApiClient.IsConnected)
+                _googleApiClient.Disconnect();
         }
 
         public void OnLocationChanged(Location location)
         {
-            throw new NotImplementedException();
+            if (location != null)
+            {
+                Log.Error(TAG, "Posisi :" + location.Altitude + ", " + location.Longitude + ", akurasi : " + location.Accuracy);
+
+                if (location.Accuracy < 500.0f)
+                {
+                    StopLocationUpdates();
+                }
+            }
+        }
+
+        protected void SendLocationDataToWebsite(Location location)
+        {
+            var dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            var date = new Date(location.Time);
+            var prefs = this.GetSharedPreferences("Lok", 0);
         }
     }
 }
