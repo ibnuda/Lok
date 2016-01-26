0e25bcc47d5fcd215881c56a22f30c71e7ced8c4
ibnuda <ibnu.d.a@student.uns.ac.id>
2016-01-26 15:39:33 +0700

    Replaced tabs with spaces.

--- Lok/LokTrackerBootReceiver.cs
+++ Lok/LokTrackerBootReceiver.cs
@@ -13,30 +13,29 @@ using Android.Widget;
 
 namespace Lok
 {
-	[Activity (Label = "LokTrackerBoot")]			
-	public class LokTrackerBootReceiver : BroadcastReceiver
-	{
-		private static readonly string TAG = "LokTrackerBoot";
-		public override void OnReceive(Context context, Intent intent)
-		{
-			var alarmManager = (AlarmManager)context.GetSystemService (Context.AlarmService);
-			var intentTracker = new Intent (context, typeof(LokTrackerAlarmReceiver));
-			var intentPending = PendingIntent.GetBroadcast (context, 0, intentTracker, 0);
+    [Activity (Label = "LokTrackerBoot")]
+    public class LokTrackerBootReceiver : BroadcastReceiver
+    {
+        private static readonly string TAG = "LokTrackerBoot";
+        public override void OnReceive(Context context, Intent intent)
+        {
+            var alarmManager = (AlarmManager)context.GetSystemService (Context.AlarmService);
+            var intentTracker = new Intent (context, typeof(LokTrackerAlarmReceiver));
+            var intentPending = PendingIntent.GetBroadcast (context, 0, intentTracker, 0);
 
-			var prefs = context.GetSharedPreferences ("lok", 0);
-			var intervalMinute = prefs.GetInt ("interval", 1);
-			var currentlyTracking = prefs.GetBoolean ("currentlyTracking", false);
+            var prefs = context.GetSharedPreferences ("lok", 0);
+            var intervalMinute = prefs.GetInt ("interval", 1);
+            var currentlyTracking = prefs.GetBoolean ("currentlyTracking", false);
 
-			if (currentlyTracking)
-				alarmManager.SetRepeating (
-					AlarmType.ElapsedRealtimeWakeup,
-                    SystemClock.ElapsedRealtime (), 
-					intervalMinute * 60000, 
-					intentPending);
-			else
-				alarmManager.Cancel (intentPending);
-		}
-			
-	}
+            if (currentlyTracking)
+                alarmManager.SetRepeating (
+                    AlarmType.ElapsedRealtimeWakeup,
+                    SystemClock.ElapsedRealtime (),
+                    intervalMinute * 60000,
+                    intentPending);
+            else
+                alarmManager.Cancel (intentPending);
+        }
+    }
 }
 
